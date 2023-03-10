69cd749f83975c44fadacb599ff112ffe5e112f7
Arthur Petrosov <arthurpetrosov@gmail.com>
2023-03-10 20:56:44 +0400

    First commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,9 @@
+public class CreditPaymentService {
+    public double calculate (double yearPr, int mouths, double summa) {
+        double mouthPr = (yearPr /mouths/ 100);
+        double mouthPay =(int) (summa * ((mouthPr * (Math.pow((1 + mouthPr), mouths) / (Math.pow((1 + mouthPr), mouths) -1)))));
+
+        return mouthPay;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+ public class Main {
+        public static void main(String[] args) {
+            CreditPaymentService service = new CreditPaymentService();
+
+            System.out.println();
+            System.out.println("На 1 год");
+            System.out.println(service.calculate(9.99, 12, 1_000_000));
+            System.out.println();
+            System.out.println("На 2 года");
+            System.out.println(service.calculate(9.99, 24, 1_000_000));
+            System.out.println();
+            System.out.println("На 3 года");
+            System.out.println(service.calculate(9.99, 36, 1_000_000));
+        }
+    }
