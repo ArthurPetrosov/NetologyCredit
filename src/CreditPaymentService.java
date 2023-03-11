805f66a96d58cfcf328579443f9becfb4d83c47d
Arthur Petrosov <arthurpetrosov@gmail.com>
2023-03-11 22:42:58 +0400

    Reformat commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-    public double calculate (double yearPr, int mouths, double summa) {
-        double mouthPr = (yearPr /mouths/ 100);
-        double mouthPay =(int) (summa * ((mouthPr * (Math.pow((1 + mouthPr), mouths) / (Math.pow((1 + mouthPr), mouths) -1)))));
+    public double calculate(double yearPr, int mouths, double summa) {
+        double mouthPr = (yearPr / mouths / 100);
+        double mouthPay = (int) (summa * ((mouthPr * (Math.pow((1 + mouthPr), mouths) / (Math.pow((1 + mouthPr), mouths) - 1)))));
 
         return mouthPay;
     }
